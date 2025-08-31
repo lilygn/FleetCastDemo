8c89cc1da74284bb13b74a5c99afe1b947c69de8
lilygniedz <lilygn6@gmail.com>
2025-08-31 16:58:54 -0500

    Modification of server backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     import.meta?.env?.VITE_API_URL ||       // Vite
-    process.env.REACT_APP_API_URL ||        // CRA
+    process.env.REACT_APP_API_URL || 
+     " https://fleetcast.onrender.com" ||
+
     'http://localhost:8080',                                    
 });
 
